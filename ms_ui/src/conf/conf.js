fc01f6204c41bfaa275c9488fc888a55c5e637d7
meis <meis5@qq.com>
2017-04-20 02:36:34 +0800

    sdfsdf

--- ms_ui/src/conf/conf.js
+++ ms_ui/src/conf/conf.js
@@ -2,13 +2,13 @@ const env = process.env.NODE_ENV || 'development'
 
 const development = {
   env,
-  port: 9999,
-  publicPath: 'http://localhost:9998',
+  port: 9995,
+  publicPath: 'http://localhost:9994',
   resourcesPath: 'cdn.bootcss.com',
 }
 
 const production = Object.assign({}, development, {
-  port: 9999,
+  port: 9995,
   publicPath: '//pbc.ms5.me/static/ms_ui/assets',
 })
 
