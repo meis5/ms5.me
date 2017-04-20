6e9a30b8af63dedf85f908214f6722cbc799d605
Syang3 <Sheldon.Yang@activenetwork.com>
2017-04-20 18:33:57 +0800

    bug: update test

--- ms_ui/src/web/routes/Router.jsx
+++ ms_ui/src/web/routes/Router.jsx
@@ -1,27 +1,26 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
 
-import styles from '../styles'
-import Header from '../components/Header'
-
-
+import HomeRoute from './home'
 import StylesRoute from './styles'
 import ElementsRoute from './elements'
 import ComponentsRoute from './components'
 
-export default class Router extends React.Component {
+export default class Router extends Component {
   render() {
     return (
       <div>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+          <li><Link to="/">home</Link></li>
+          <li><Link to="/styles">Styles</Link></li>
+          <li><Link to="/elements">Elements</Link></li>
+          <li><Link to="/components">Components</Link></li>
         </ul>
-
         <hr />
-
+        <Route exact path="/" component={HomeRoute} />
         <Route exact path="/" component={StylesRoute} />
         <Route path="/about" component={ElementsRoute} />
+        <Route exact path="/" component={ComponentsRoute} />
       </div>
     )
   }
